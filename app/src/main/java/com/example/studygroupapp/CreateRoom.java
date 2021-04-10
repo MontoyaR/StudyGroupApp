7b5629fb66e036cf314a1cc28506db8336964591
MontoyaR <42822862+MontoyaR@users.noreply.github.com>
2021-04-10 16:55:39 -0400

    Create Room update

--- app/src/main/java/com/example/studygroupapp/CreateRoom.java
+++ app/src/main/java/com/example/studygroupapp/CreateRoom.java
@@ -1,4 +1,14 @@
 package com.example.studygroupapp;
 
-public class CreateRoom {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createroom);
+    }
 }
--- app/src/main/java/com/example/studygroupapp/Dashboard.java
+++ app/src/main/java/com/example/studygroupapp/Dashboard.java
@@ -1,14 +1,32 @@
 package com.example.studygroupapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Dashboard extends AppCompatActivity {
 
+    Button createRoom;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        createRoom = (Button) findViewById(R.id.btnCreateRoom);
+
+        // Login functionality
+        createRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CreateRoom.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
